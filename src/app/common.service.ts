585096ba513d794a4bd078b4d3a7975abe08b36e
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2018-12-04 07:00:19 +0100

    Added some

---
+++ src/app/common.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, RequestOptions } from '@angular/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommonService {
+
+  constructor() { }
+}
