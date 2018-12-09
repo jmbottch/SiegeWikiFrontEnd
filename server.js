9fa8839abe29a9667a9e72b5b64a02a390107c25
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2018-12-09 21:32:51 +0100

    troubleshoot heroku

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(__dirname + '/dist/frontendapp'));
+app.use(express.static(__dirname + '/frontendapp'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname + '/dist/frontendapp/src/index.html'));
+  res.sendFile(path.join(__dirname + '/frontendapp/src/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
