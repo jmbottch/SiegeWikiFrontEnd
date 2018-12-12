0046c67698216e21375bd7af85595178da1063db
Jeroen Bottcher <jm.bottcher@student.avans.nl>
2018-12-12 06:42:21 +0100

    c

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SeasonsComponent } from './seasons/seasons.component';
@@ -35,9 +35,6 @@ import { SeasonPopulateComponent } from './seasons/season-populate/season-popula
 
 
 @NgModule({
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ],
 
   declarations: [
     AppComponent,
